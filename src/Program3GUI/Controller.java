0da5de0aba411bc16cbea6e4f2b822c3d11eceea
Rizwan Chowdhury <rizwanchowdhury111@gmail.com>
2020-03-16 14:18:56 -0400

    Added skeletons for some of the gui functinalities, which need to be filled in

--- src/Program3GUI/Controller.java
+++ src/Program3GUI/Controller.java
@@ -7,7 +7,14 @@ import javafx.scene.layout.HBox;
 import javafx.event.ActionEvent;
 import javafx.scene.control.TextArea;
 
+/**
+ * Controller class used to define and control the functionalities of the GUI. Will coordinate with FXML file.
+ * @author Rizwan Chowdhury
+ * @author Tin Fung
+ */
 public class Controller {
+
+    //following are instances of the fxml elements, using their fxml ids, that will be used
     @FXML
     private HBox triStateSelectionArea;
 
@@ -24,9 +31,18 @@ public class Controller {
     private ToggleGroup exchangeRadioGroup;
 
 
+    /**
+     * Controller Method to disable/enable different sections of student specific information input based on which
+     * option (type of student) is selected. Only section corresponding to selected option (Instate, Outstate, International)
+     * will be enabled, others will be disabled.
+     * @param event Toggling of Instate, Outstate, or International to trigger this function.
+     * @author Rizwan Chowdhury
+     */
     @FXML
     public void typeSelectionDisabler(ActionEvent event){
-        String eventSourceId = ((RadioButton)event.getSource()).getId();
+        String eventSourceId = ((RadioButton)event.getSource()).getId(); // get which button was toggled
+
+        //based on toggled button carry out appropriate action
         switch (eventSourceId){
             case "instateRadioButton":
                 triStateSelectionArea.setDisable(true);
@@ -46,6 +62,47 @@ public class Controller {
                 exchangeSelectionArea.setDisable(false);
                 break;
         }
+    }
+
+
+    /**
+     * The action(s) that will be performed when the add button is pressed, mainly to add student to TuitionManager list.
+     * @param event Button press of Add button in the GUI
+     * @author Rizwan Chowhdury
+     */
+    @FXML
+    public void actionWhenAddButtonPressed(ActionEvent event){
+        //- Will check for proper input format.
+        //- Will check if information inputted obeys the rules of type of student selected.
+        //- Will check if Student is already in list, if not then finally add the student.
+
+
+    }
+
+
+    /**
+     * Remove student from running list when the Remove button is pressed.
+     * @param event Pressing of the Remove button
+     * @author Rizwan Chowdhury
+     * @author Tin Fung
+     */
+    @FXML
+    public void actionWhenRemoveButtonPressed(ActionEvent event){
+        // - Check if inputted information is in correct format (maybe)
+        // - Check if list is empty, if it is will print no student/else go to next step
+        // - Check if inputted student exists in list, if yes then remove else print student not in list
+    }
+
+
+    /**
+     * Prints all the student to the output textbox area when "Print" button is pressed.
+     * @param event Print button being pressed
+     */
+    @FXML
+    public void actionWhenPrintButtonPressed(ActionEvent event){
+        // -Will check if list is empty, if yes then print list empty else go to next line
+        // -Will print all the students in list using the StudentList print() function
+        // -Will print --end of list-- at the end
     }
 
 }
